31a610cf22ae0ed35bed312f90204b3787366b0d
James Denman <james@lattapartners.com>
2014-09-16 12:27:10 -0400

    update recipe to find first instance in layer

--- erlang/recipes/build_cluster.rb
+++ erlang/recipes/build_cluster.rb
@@ -1,6 +1,6 @@
 execute 'build_cluster' do
   command "echo 'whoami; whoami'"
   action :run
-  Chef::Log.info("#{node[:opsworks][:layers].map{|layer| layer[:name]}}")
+  Chef::Log.info("#{node[:opsworks][:layers]['rabbitmq-base-node']}")
   
 end

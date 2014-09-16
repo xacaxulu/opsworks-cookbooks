76816060d71b22e6d42c2226d14d715b7641bd37
James Denman <james@lattapartners.com>
2014-09-16 16:45:33 -0400

    update recipe to find first instance in layer

--- erlang/recipes/build_cluster.rb
+++ erlang/recipes/build_cluster.rb
@@ -1,15 +1,15 @@
 execute 'build_cluster' do
   command "echo"
   action :run
+  
   instance_keys = node[:opsworks][:layers]['rabbitmq-base-node'][:instances].keys
-  instances = node[:opsworks][:layers]['rabbitmq-base-node'][:instances]
   base_node = node[:opsworks][:layers]['rabbitmq-base-node'][:instances][instance_keys.first]
-  Chef::Log.info("#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first}")
-  Chef::Log.info("#{instance_keys.first}")
-  Chef::Log.info("#{instance_keys.last}")
-  Chef::Log.info("#{base_node}")
+  
   instance_keys.each do |key|
-    if node[:opsworks][:layers]['rabbitmq-base-node'][:instances][key]
+    Chef::Log.info("#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances][key].first}")
+    Chef::Log.info("#{base_node}")
+    Chef::Log.info("#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances][key].first != base_node}")
+    if node[:opsworks][:layers]['rabbitmq-base-node'][:instances][key].first != base_node
       command "sudo rabbitmqctl stop_app && sudo rabbitmqctl join_cluster rabbit@#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first} &&
       sudo rabbitmqctl start_app"
       action :run

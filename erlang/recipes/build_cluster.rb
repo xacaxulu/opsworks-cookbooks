9a3230bbe382f772d3ca97ce643f911239009ece
James Denman <james@lattapartners.com>
2014-09-16 13:38:11 -0400

    update recipe to find first instance in layer

--- erlang/recipes/build_cluster.rb
+++ erlang/recipes/build_cluster.rb
@@ -1,14 +1,13 @@
 execute 'build_cluster' do
-  Chef::Log.info("#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances]}")
-  Chef::Log.info("#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances].keys}")
   instance_keys = node[:opsworks][:layers]['rabbitmq-base-node'][:instances].keys
   instances = node[:opsworks][:layers]['rabbitmq-base-node'][:instances]
   base_node = node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first
-  instance_keys.each do |key|
-    if node[:opsworks][:layers]['rabbitmq-base-node'][:instances].key.first.first != node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first
-      command "sudo rabbitmqctl stop_app && sudo rabbitmqctl join_cluster rabbit@#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first} &&
-      sudo rabbitmqctl start_app"
-      action :run
-    end
-  end
+  Chef::Log.info("#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances][instance_keys.first]}")
+  # instance_keys.each do |key|
+  #   if node[:opsworks][:layers]['rabbitmq-base-node'][:instances].key
+  #     command "sudo rabbitmqctl stop_app && sudo rabbitmqctl join_cluster rabbit@#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first} &&
+  #     sudo rabbitmqctl start_app"
+  #     action :run
+  #   end
+  # end
 end

5ae145b33f65df67160dd45e02985b87384303eb
James Denman <james@lattapartners.com>
2014-09-16 18:45:36 -0400

    update node[:rabbit]

--- erlang/recipes/start_detached_server.rb
+++ erlang/recipes/start_detached_server.rb
@@ -2,5 +2,5 @@ execute "start_detached_server" do
   Chef::Log.info("#{node[:opsworks][:stack][:name]}")
   Chef::Log.info("starting detached rabbitmq-server")
   command "sudo rabbitmq-server -detached"
-  action :run
+  not_if node[:rabbit]
 end

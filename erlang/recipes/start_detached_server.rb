4454068a79a5d552b3e7b91fc006461b0a952517
James Denman <james@lattapartners.com>
2014-09-16 10:04:50 -0400

    trying wout sudo

--- erlang/recipes/start_detached_server.rb
+++ erlang/recipes/start_detached_server.rb
@@ -1,5 +1,7 @@
 execute "start_detached_server" do
-  Chef::Log.info('heyyy')
+  Chef::Log.info("node[:opsworks][:stack][:name]")
+  puts "node[:opsworks][:stack][:name]"
+  command "whoami"
   command "sudo rabbitmq-server -detached"
   action :run
 end

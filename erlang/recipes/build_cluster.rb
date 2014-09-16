8be8dd86546866e8ab99206a99673a8ebe6d9a0d
James Denman <james@lattapartners.com>
2014-09-16 14:33:55 -0400

    update recipe to find first instance in layer

--- erlang/recipes/build_cluster.rb
+++ erlang/recipes/build_cluster.rb
@@ -3,10 +3,11 @@ execute 'build_cluster' do
   action :run
   instance_keys = node[:opsworks][:layers]['rabbitmq-base-node'][:instances].keys
   instances = node[:opsworks][:layers]['rabbitmq-base-node'][:instances]
-  base_node = node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first.first
-  Chef::Log.info("#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first}")
+  base_node = node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first
+  Chef::Log.info("#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first}")
   Chef::Log.info("#{instance_keys.first}")
   Chef::Log.info("#{instance_keys.last}")
+  Chef::Log.info("#{base_node}")
   # instance_keys.each do |key|
   #   if node[:opsworks][:layers]['rabbitmq-base-node'][:instances].key
   #     command "sudo rabbitmqctl stop_app && sudo rabbitmqctl join_cluster rabbit@#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first} &&

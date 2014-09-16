d923931e50c96d48402f1a662f604e204802e5e4
James Denman <james@lattapartners.com>
2014-09-16 13:32:07 -0400

    update recipe to find first instance in layer

--- erlang/recipes/build_cluster.rb
+++ erlang/recipes/build_cluster.rb
@@ -4,11 +4,11 @@ execute 'build_cluster' do
   instance_keys = node[:opsworks][:layers]['rabbitmq-base-node'][:instances].keys
   instances = node[:opsworks][:layers]['rabbitmq-base-node'][:instances]
   base_node = node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first
-  instance_keys.each_with_index do |index, key|
-    next if index == 0
-    command "sudo rabbitmqctl stop_app && 
-    sudo rabbitmqctl join_cluster rabbit@#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first} &&
-    sudo rabbitmqctl start_app"
-    action :run
+  instance_keys.each do |key|
+    if node[:opsworks][:layers]['rabbitmq-base-node'][:instances].key.first.first != node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first
+      command "sudo rabbitmqctl stop_app && sudo rabbitmqctl join_cluster rabbit@#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first} &&
+      sudo rabbitmqctl start_app"
+      action :run
+    end
   end
 end

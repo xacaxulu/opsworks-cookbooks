eb617b1c256e7ceb63baf0a5223a67df72edb1b7
James Denman <james@lattapartners.com>
2014-09-16 22:10:08 -0400

    update node[:rabbit]

--- erlang/recipes/ha_policy_all.rb
+++ erlang/recipes/ha_policy_all.rb
@@ -1,6 +1,5 @@
 execute "ha_policy_all" do
   Chef::Log.info("setting ha policy to ha-all...")
-  pattern = /^*\./
-  command "rabbitmqctl set_policy ha-all '#{pattern}' '{'ha-mode':'all'}'"
+  command "rabbitmqctl set_policy ha-all /^*\./ '{'ha-mode':'all'}'"
   action :run
 end

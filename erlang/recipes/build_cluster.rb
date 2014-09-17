292c3437b541f6651b821dda9da6414b561f5c5e
James Denman <james@lattapartners.com>
2014-09-16 21:29:58 -0400

    update node[:rabbit]

--- erlang/recipes/build_cluster.rb
+++ erlang/recipes/build_cluster.rb
@@ -1,12 +1,7 @@
 execute 'build_cluster' do
-  instance_keys.each do |key|
-    Chef::Log.info("#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances][key].first}")
-    Chef::Log.info("#{base_node}")
-    Chef::Log.info("#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances][key].first != base_node}")
-      command "rabbitmqctl stop_app && rabbitmqctl join_cluster rabbit@#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first} &&
-      rabbitmqctl start_app"
-      action :run
-      only_if "rabbitmqctl status", :returns => 0
-    end
-  end
+  command "rabbitmqctl stop_app && 
+  rabbitmqctl join_cluster rabbit@#{node[:opsworks][:layers]['rabbitmq-base-node'][:instances].first.first} &&
+  rabbitmqctl start_app"
+  action :run
+  only_if "rabbitmqctl status", :returns => 0
 end

3056f627b763d5bd0edb6a175f5e586342700980
James Denman <james@lattapartners.com>
2014-09-16 21:10:32 -0400

    update node[:rabbit]

--- erlang/recipes/start_detached_server.rb
+++ erlang/recipes/start_detached_server.rb
@@ -1,5 +1,5 @@
 bash "start_detached_server" do
-  only_if "rabbitmqctl status", :returns => 2
-  command 'rabbitmq-server -detached'
+  not_if "sudo rabbitmqctl status", :returns => 0
+  command 'sudo rabbitmq-server -detached'
   action :run
 end

2e1c3bc77c4e82e56e39aafc8b21709a001fe848
James Denman <james@lattapartners.com>
2014-09-16 20:14:33 -0400

    update node[:rabbit]

--- erlang/recipes/start_detached_server.rb
+++ erlang/recipes/start_detached_server.rb
@@ -4,7 +4,6 @@ execute "start_detached_server" do
   Chef::Log.info(node[:hostname])
   Chef::Log.info(node[:current_user])
   Chef::Log.info(node[:chef_environment])
-  Chef::Log.info(node[:chef_environment].keys)
   Chef::Log.info(node[:opsworks].keys)
   # command "sudo rabbitmq-server -detached"
   command 'whoami'
